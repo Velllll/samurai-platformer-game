460283743255992d7617deec830ecfca28d57651
Ilya Velo <bencubeincube@gmail.com>
2023-01-19 13:45:56 +0300

    fullscreen

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ window.addEventListener('load', () => {
       game = new Game(canvasSettings)
     }
   })
+  document.getElementById('fullscreen').addEventListener('click', () => {
+    menu.toggleFullScreen()
+  })
 
   let lastTimeStamp = 0
   function animate(timeStamp) {
